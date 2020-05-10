5fcef704a5b223b17fce06dde5753093bf01965e
Bryant Luk <bryant.luk@bryantluk.com>
2020-05-10 14:44:23 -0500

    Add /version route

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,24 @@
 // except according to those terms.
 
 use log::info;
+use serde::Serialize;
 use std::env;
 use tide::{Request, Response, StatusCode};
 
 use gargantua::{log_request::LogRequest, request_id::RequestIdMiddleware};
+
+#[derive(Debug, Serialize)]
+struct VersionResponse {
+    commit: String,
+}
+
+impl VersionResponse {
+    fn new() -> Self {
+        let commit = env::var("SOURCE_COMMIT").unwrap_or_else(|_| String::from(""));
+
+        Self { commit }
+    }
+}
 
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -22,11 +36,17 @@ async fn main() -> Result<(), std::io::Error> {
     let mut app = tide::new();
     app.middleware(RequestIdMiddleware)
         .middleware(LogRequest)
-        .at("/")
+        .at("")
         .get(|_: Request<()>| async move { Ok(Response::new(StatusCode::NotFound)) })
         .all(|_: Request<()>| async move { Ok(Response::new(StatusCode::MethodNotAllowed)) })
         .at("/*")
-        .get(|_: Request<()>| async move { Ok(Response::new(StatusCode::NotFound)) })
+        .get(|req: Request<()>| async move {
+            let path = req.uri().path();
+            match path {
+                "/version" => Ok(Response::new(StatusCode::Ok).body_json(&VersionResponse::new())?),
+                _ => Ok(Response::new(StatusCode::NotFound)),
+            }
+        })
         .all(|_: Request<()>| async move { Ok(Response::new(StatusCode::MethodNotAllowed)) });
 
     info!("Application starting on port: {}", port);
